1e09046abe81c4b580bb0ac8a42bb4d75ca23560
CR7sewey <miguelmotacarvalho@gmail.com>
2024-05-11 16:52:06 +0100

    Refactoring User model

--- e-commerce-api-nodejs/models/User.js
+++ e-commerce-api-nodejs/models/User.js
@@ -36,6 +36,12 @@ const UserSchema = mongoose.Schema(
       enum: ["admin", "user"],
       default: "user",
     },
+    verificationToken: String,
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    verified: Date,
   },
   { timestamps: true }
 );
