e88a5c672adf30eaf32772399113711da368706b
CR7sewey <miguelmotacarvalho@gmail.com>
2024-05-12 10:49:10 +0100

    URL correction

--- e-commerce-api-nodejs/utils/sendVerificationEmail.js
+++ e-commerce-api-nodejs/utils/sendVerificationEmail.js
@@ -10,7 +10,7 @@ const sendVerificationEmail = async ({
   // First -> in the html we will send an url for veryfing email. For this, from
   //the frontend, we want the user to click the link (link in the frontend that consumes verify-email) and be redirected to some
   //url to verify the email, which is our route verify-email
-  const verifyEmailURL = `${origin}/user/verify-email?/token=${verificationToken}&email=${email}`;
+  const verifyEmailURL = `${origin}/user/verify-email?token=${verificationToken}&email=${email}`;
   const emailMessage = `<p>Hi there, ${name}. Please click in the link bellow to activate have access to your account. <br /> <a href=${verifyEmailURL}> Verify Email</a> `;
 
   await sendEmail({
